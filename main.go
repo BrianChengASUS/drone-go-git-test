aabfde375e6baf427e3bb54e6dfd5e7980f8ae6d
brian_cheng <brian_cheng@asus.com>
2019-12-30 17:41:27 +0800

    go

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello World Drone Go")
+	aSecret := os.Getenv("A_SECRET")
+	fmt.Println(aSecret)
+	fmt.Println(reverseString(aSecret))
+}
+
+func reverseString(s string) string {
+	r := []rune(s)
+	sb := strings.Builder{}
+	for i := len(r) - 1; i >= 0; i-- {
+		sb.WriteRune(r[i])
+	}
+	return sb.String()
 }
